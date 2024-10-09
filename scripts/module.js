3487f378a5b57324c60545a4515522a2da5cb896
sreegs <kenkrzeminski+secondary@gmail.com>
2024-10-09 16:59:41 -0400

    Fixing whitespace

--- scripts/module.js
+++ scripts/module.js
@@ -1,48 +1,48 @@
 import { DiceSystem } from '../../dice-so-nice/api.js';
 
 Hooks.on('diceSoNiceReady', (dice3d) => {
-const mySystem = new DiceSystem("ta-dice", "Triangle Agency", "preferred", "Triangle Man");
+  const mySystem = new DiceSystem("ta-dice", "Triangle Agency", "preferred", "Triangle Man");
 
-dice3d.addSystem(mySystem)
+  dice3d.addSystem(mySystem)
 
-// add d4, d6, d8, d10, d20
-dice3d.addDicePreset({
-  type: "d4",
-  labels: [
-    "", "", "3", ""
-  ],
-  system: "ta-dice"
-})
+  // add d4, d6, d8, d10, d20
+  dice3d.addDicePreset({
+    type: "d4",
+    labels: [
+      "", "", "3", ""
+    ],
+    system: "ta-dice"
+  })
 
-dice3d.addDicePreset({
-  type: "d6",
-  labels: [
-    "1", "2", "3", "4", "5", "6"
-  ],
-  system: "ta-dice"
-})
+  dice3d.addDicePreset({
+    type: "d6",
+    labels: [
+      "1", "2", "3", "4", "5", "6"
+    ],
+    system: "ta-dice"
+  })
 
-dice3d.addDicePreset({
-  type: "d8",
-  labels: [
-    "1", "2", "3", "4", "5", "6", "7", "8"
-  ],
-  system: "ta-dice"
-})
+  dice3d.addDicePreset({
+    type: "d8",
+    labels: [
+      "1", "2", "3", "4", "5", "6", "7", "8"
+    ],
+    system: "ta-dice"
+  })
 
-dice3d.addDicePreset({
-  type: "d10",
-  labels: [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
-  ],
-  system: "ta-dice"
-})
+  dice3d.addDicePreset({
+    type: "d10",
+    labels: [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    ],
+    system: "ta-dice"
+  })
 
-dice3d.addDicePreset({
-  type: "d20",
-  labels: [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
-  ],
-  system: "ta-dice"
-})
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
+    ],
+    system: "ta-dice"
+  })
 })
